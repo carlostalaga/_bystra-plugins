28a2efd819a6ffa613f374b301a9d5f2f5a6f607
carlostalaga <cat@carlostalaga.com.au>
2024-07-08 13:18:33 +0930

    chore: Clean the scripts areas from bystra-analytics.php

--- bystra-analytics/bystra-analytics.php
+++ bystra-analytics/bystra-analytics.php
@@ -18,24 +18,7 @@ function bystra_header_scripts() {
 /* Your header scripts go here */
 ?>
 
-<!-- Google Tag Manager -->
-<script>
-(function(w, d, s, l, i) {
-    w[l] = w[l] || [];
-    w[l].push({
-        'gtm.start': new Date().getTime(),
-        event: 'gtm.js'
-    });
-    var f = d.getElementsByTagName(s)[0],
-        j = d.createElement(s),
-        dl = l != 'dataLayer' ? '&l=' + l : '';
-    j.async = true;
-    j.src =
-        'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-    f.parentNode.insertBefore(j, f);
-})(window, document, 'script', 'dataLayer', 'GTM-P3MLDVVW');
-</script>
-<!-- End Google Tag Manager -->
+
 
 <?php
 }
@@ -47,9 +30,7 @@ function bystra_body_open_scripts() {
 /* Your code to be inserted just after the opening of the body tag goes here */
 ?>
 
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P3MLDVVW" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+
 
 <?php
 }
